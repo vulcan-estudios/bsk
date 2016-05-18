77e7ba3adf9bab28b46953e40ecc27d9c8a1c8d1
Iván Meléndez <argordmel@yahoo.es>
2016-05-17 20:36:35 -0500

    Zurb

---
+++ src/app/libs/zurb/Zurb.js
@@ -0,0 +1,40 @@
+/**
+ * Utility for fundation
+ *
+ */
+
+var Abide       = require('libs/zurb/components/abide');
+
+module.exports  = {
+
+    /**
+     * Init
+     */
+    init: function() {
+
+        $(document).foundation();
+
+        // Abide
+        this.abide();
+
+    },
+
+    reflow: function() {
+
+        // Foundation.reInit() ?
+        Foundation.reflow();
+
+    },
+
+    /**
+     * Custom utility for abide
+     */
+    abide: function() {
+
+        // Merged
+        Foundation.Abide.defaults.patterns      = _.extend(Foundation.Abide.defaults.patterns,      Abide.patterns);
+        Foundation.Abide.defaults.validators    = _.extend(Foundation.Abide.defaults.validators,    Abide.validators);
+
+    }
+
+};
