9a32e2b9e6444cdab1e70fed88abe3779f9cad5b
Iván Meléndez <argordmel@yahoo.es>
2016-05-23 14:59:58 -0500

    Zurb

--- src/app/bootstrap/AppBootstrap.js
+++ src/app/bootstrap/AppBootstrap.js
@@ -12,14 +12,11 @@ var AppModel        = require('./AppModel');
 var Flash           = require('helpers/flash/flash');
 
 // Libs
-var Zurb            = require('libs/zurb/Zurb');
 var Filter          = require('libs/filter/Filter');
 
 module.exports = function() {
 
     var Router      = Backbone.Router.extend(AppRouter);
-
-    Zurb.init();
 
     return { Config: Config, Flash: Flash.init(), Router: new Router(), Model: new AppModel(), View: AppView, Filter: Filter };
 
