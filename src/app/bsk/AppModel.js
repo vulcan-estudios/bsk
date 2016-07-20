2f3878c0f3f10d903ba81186bcef41b4fe677418
Iván Meléndez <argordmel@yahoo.es>
2016-07-20 18:09:58 -0500

    Models to objects and folders

--- src/app/bsk/AppModel.js
+++ src/app/bsk/AppModel.js
@@ -4,22 +4,31 @@
  * @type type
  */
 
-var Models  = require('bulk-require')(__dirname + '/..', ['./models/**/*.js']).models;
+var TmpFolders  = require('bulk-require')(__dirname + '/..', ['./models/**/*.js']).models   || {};
+var Models      = require('bulk-require')(__dirname + '/..', ['./models/*.js']).models      || {};
+
+// Put only folders with objects
+var Folders     = {};
+for(let i in TmpFolders) {
+    if(Models[i] === undefined) {
+        Folders[i]  = TmpFolders[i];
+    }
+}
 
 module.exports  = function() {
 
     var objs    = {};
 
-    _(Models).each(function(model, key) {
+    var toBackboneModel = function(model, key, folder) {
 
-        var tmpModel            = Backbone.Model.extend(_({}).extend(model, {
+        var tmpModel    = Backbone.Model.extend(_({}).extend(model, {
 
             objCollection: null,
 
             setCollection: function(data) {
                 var _this           = this;
                 var tmpCollection   = Backbone.Collection.extend({
-                    model: Backbone.Model.extend(model)
+                    model: !folder ? Backbone.Model.extend(model) : Backbone.Model.extend(folder[key])
                 });
                 _this.objCollection = new tmpCollection(data);
                 return true;
@@ -31,7 +40,29 @@ module.exports  = function() {
 
         }));
 
-        objs[key]       = new tmpModel();
+        return new tmpModel();
+
+    };
+
+    // Each Models
+    _(Models).each(function(model, key) {
+
+        objs[key]    = toBackboneModel(model, key);
+
+    });
+
+    // Each Folders
+    _(Folders).each(function(models, folder) {
+
+        // Set folder
+        objs[folder] = {};
+
+        // Each Models
+        _(models).each(function(model, key) {
+
+            objs[folder][key]   = toBackboneModel(model, key, folder);
+
+        });
 
     });
 
